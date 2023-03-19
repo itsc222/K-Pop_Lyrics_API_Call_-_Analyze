2f5339b1904dffa748b25fb0ccafc7d5d2562ecf
Ian Schneider <122047467+itsc222@users.noreply.github.com>
2023-03-19 21:04:11 +0000

    I edited this proof of concept for calling the top 100 songs in Japan and tokenizing them.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Import Packages
 
-import pandas as pd
+import polars as pl
 import json
 from musixmatch import Musixmatch
+
 musixmatch = Musixmatch('915a5145eef8a1ac16c4a7a69aa8f81c')
 
 import os
@@ -11,151 +12,186 @@ from nltk.tokenize import RegexpTokenizer
 from collections import Counter
 import py3langid as langid
 
-#Let's start with the albumid. You can find this on musixmatch by looking at the source code on Chrome.
+#This is an empty data set to create the columns of our main mega_meta_df
+mega_meta_data = {
+  "track_name": [],
+  "artist_name": [],
+  "release_date": [],
+  "track_id": []
+}
 
-albumid = 27336583
+#This is an empty data set to creat columns of our all_word_df
+all_word_df_data = {"track_id": [],
+                    "word": [], 
+                    "lang_id": [], 
+                    "lang_val": []}
 
-#Let's import the album metadata to find the artist name and release date. We'll refer to this later.
+#This is our all_word_df
+all_word_df = pl.DataFrame(all_word_df_data,
+                  schema={"track_id": pl.Int64,
+                          "word": pl.Utf8,
+                          "lang_id": pl.Utf8,
+                          "lang_val": pl.Float64})
 
-album_data = musixmatch.album_get(albumid)
-album_meta = album_data["message"]["body"]["album"]
+#Now let's make the mega_meta_df
+mega_meta_df = pl.DataFrame(mega_meta_data,
+                            schema={
+                              "track_name": pl.Utf8,
+                              "artist_name": pl.Utf8,
+                              "release_date": pl.Utf8,
+                              "track_id": pl.Int64
+                            })
 
-#Print album metadata for audit in case there is problems with the code.
-# print(album_meta)
+#Let's also keep an empty list for collecting track_ids for collecting lyrics later.
+track_id_collection = []
+track_id_collection_len = len(track_id_collection)
 
-#Let's pull out the artist_name and release_date for our dataframe later.
+#Let's start with the chart. You can find this on musixmatch by looking at the source code on Chrome.
+chart = musixmatch.chart_tracks_get(1, 100, f_has_lyrics=True, country='jp')
 
-artist_name = (album_meta["artist_name"])
-print(artist_name)
-release_date = (album_meta["album_release_date"])
-print(release_date)
+#We need the length of the number of tracks in order to index in a FOR loop.
+chart_count = len(chart["message"]["body"]["track_list"])
+chart_count_index = chart_count - 1
 
-#Now let's import a tracklist of album_id's from the album.
-get_album_tracks = musixmatch.album_tracks_get(albumid, 0, 100, '')
-get_album = get_album_tracks["message"]["body"]["track_list"]
+#index for future FOR loop.
+index = list(range(chart_count))
+# print(index)
 
-#This should help us pull out the song title of the first song of the album.
-get_first_song_title = get_album[0]["track"]["track_name"]
+for i in index:
 
-#Let's keep the track title saved for later to add to our dataframe.
-track_title = get_first_song_title
-print(track_title)
-print('\n')
+  chart_message = chart["message"]["body"]["track_list"][i]["track"]
 
-#This helps us extract the song
-track_id = get_album[0]["track"]["track_id"]
-  
-# print(track_id)
-#This should be a list of track_id's from the imported album.abs
+  #We need the track ID
+  track_id = chart_message["track_id"]
+  # print(track_id)
+  track_id_collection.append(track_id)
 
-#Now, let's find the lyrics. We will use the index[0] since most K-pop albums put their lead single in first position.
+  #We need the track name.
+  track_name = chart_message["track_name"]
+  # print(track_name)
 
-import_lyrics = musixmatch.track_lyrics_get(track_id)
+  #We need the artist name.
+  artist_name = chart_message["artist_name"]
+  # print(artist_name)
 
-#Then let's extract and trim the lyrics.
+  #API Call for Album Metadata so we can add the release date for the song.
+  album_id = chart_message["album_id"]
+  album_data = musixmatch.album_get(album_id)
+  album_message = album_data["message"]["body"]["album"]
 
-lyrics = import_lyrics["message"]["body"]["lyrics"]["lyrics_body"]
-lyrics_trim = lyrics[:-74]
-print(lyrics_trim)
+  date = album_message["album_release_date"]
+  # print(date)
 
+  metadata = {
+    "track_id": [track_id],
+    "track_name": [track_name],
+    "artist_name": [artist_name],
+    "release_date": [date]
+  }
+  # print(metadata)
 
-#Now let's write the lyrics into a .txt file in a folder called '/lyrics_txt'
+  #Build a dataframe for the track metadata.
+  metadf = pl.DataFrame(metadata,
+                        schema={
+                          "track_name": pl.Utf8,
+                          "artist_name": pl.Utf8,
+                          "release_date": pl.Date,
+                          "track_id": pl.Int64
+                        })
 
-with open(f'lyrics_txt/{artist_name}-{track_title}.txt', 'w') as f:
-    f.write(lyrics_trim)
-    f.write('\n')
+  mega_meta_df.extend(metadf)
 
-path = f'lyrics_txt/{artist_name}-{track_title}.txt'
-# print(release_date)
+# mega_meta_df.write_csv('meta_df.csv', sep=',')
 
-
-
+#This is to tokenize the songs later.
 punc_tokenizer = nltk.RegexpTokenizer(r"\w+")
 
-def k_pop_counter(path):
+for id in track_id_collection:
 
-  with open(path) as f:
-    rawline = [word.lower() for line in f for word in line.split()]
+  #Now, let's find the lyrics. We will use the index[0] since most K-pop albums put their lead single in first position.
 
-  a = []
-  for word in rawline:   
-    a.append(word.replace('(','').replace(')','') ) 
+  import_lyrics = musixmatch.track_lyrics_get(id)
 
-  b = []
-  for word in a:
-    b.append(word.replace(',','')) 
+  #Then let's extract and trim the lyrics.
 
-  a = []
-  for word in b:
-    a.append(word.replace('’',''))
+  lyrics = import_lyrics["message"]["body"]["lyrics"]["lyrics_body"]
+  lyrics_trim = lyrics[:-74]
 
-  b = []
-  for word in a:
-    b.append(word.replace('"','')) 
+  # print(lyrics_trim)
 
-  c = [punc_tokenizer.tokenize(line) for line in b]
+  a = lyrics_trim.lower()
 
-  a = []
-  for word in c:
-    for line in word:
-      a.append(line)
-
-  set1 = set(a)
-
-  langid.set_languages(['en', 'ko'])
-
-  langs_tok = []
-  for word in a:
-    langs_tok.append(langid.classify(word)[0])
-
-  langs_tok_edit = []
-  for word in langs_tok:
-    if word == 'ko':
-      langs_tok_edit.append(word.replace('ko', 'ko_tok'))
-    if word == 'en':
-      langs_tok_edit.append(word.replace('en', 'en_tok'))
+  b = ' '.join(a.splitlines())
+  # print(q)
   
-  langs_type = []
-  for word in set1:
-     langs_type.append(langid.classify(word)[0])
-    
-  langs_type_edit = []
-  for word in langs_type:
-    if word == 'ko':
-      langs_type_edit.append(word.replace('ko', 'ko_type'))
-    if word == 'en':
-      langs_type_edit.append(word.replace('en', 'en_type'))
+  c = b.split(' ')
+  # print(z)
+
   
-  type_count = Counter(langs_type_edit)
-  tok_count = Counter(langs_tok_edit)
+  d = [punc_tokenizer.tokenize(word) for word in c]
+  e = []
+  for list in d:
+    e.append(''.join(list))
+
+  f = [i for i in e if i]
+  
     
-  dict1 = dict(type_count)
-  dict2 = dict(tok_count)
-
-  dict1.update(dict2)
-  return(dict1)
-
-output = (k_pop_counter(path))
-print(output)
-
-en_tok = output["en_tok"]
-en_type = output["en_type"]
-ko_tok = output["ko_tok"]
-ko_type = output["ko_type"]
 
 
-d = {'track_title':[track_title],'release_date':[release_date], 'artist_name':[artist_name], 'ko_tok':ko_tok, 'en_tok': en_tok, 'ko_type':ko_type, 'en_type': en_type}
-df = pd.DataFrame(data = d)
+  # print(lyrics_trim)
 
-df.to_csv(f'dfs/{artist_name}_{track_title}typetok.csv')
+  # Now let's write the lyrics into a .txt file in a folder called '/lyrics_txt'
 
-# Give the directory you wish to iterate through
-my_dir = "/home/runner/MusixMatchPlayTime/dfs"
 
-# Using os.listdir to create a list of all of the files in dir
-dir_list = os.listdir(my_dir)
 
-df_list = [pd.read_csv(f'dfs/{f}') for f in dir_list]
+  #Let's import the album metadata to find the artist name and release date. We'll refer to this later.
 
-df_main = pd.concat(df_list, ignore_index = True)
-df_main.to_csv('k_pop_tok_type.csv', sep=',')
+
+  langid.set_languages(['en', 'ko', 'es', 'ja'])
+
+  for word in f:
+
+    data2 = pl.DataFrame({
+       "track_id": [id],
+        "word": [word],
+        "lang_id": [langid.classify(word)[0]],
+        "lang_val": [langid.classify(word)[1]]
+      })
+    data = {"track_id": [], 
+            "word": [], 
+            "lang_id": [], 
+            "lang_val": []}
+
+    df = pl.DataFrame(data,
+                       schema={
+                          "track_id": pl.Int64,
+                         "word": pl.Utf8,
+                         "lang_id": pl.Utf8,
+                         "lang_val": pl.Float64
+                       }
+                     )
+      # print(data2)
+    df.extend(data2)
+      # print(df)
+    all_word_df.extend(df)
+
+
+# Importing the os module
+# import os
+
+# # Give the directory you wish to iterate through
+# my_dir = "/home/runner/MusixMatchAPIwithTokenization/lyrics_txt"
+
+# # Using os.listdir to create a list of all of the files in dir
+# dir_list = os.listdir(my_dir)
+# # print(dir_list)
+
+# # Use the for loop to iterate through the list you just created, and open the files
+
+  
+
+# all_word_df.write_csv('all_word_df.csv', sep=',')
+
+full_data_df = mega_meta_df.join(all_word_df, on = "track_id")
+
+full_data_df.write_csv('full_data_df.csv', sep=',')
